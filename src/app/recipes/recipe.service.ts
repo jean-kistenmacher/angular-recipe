f0a5341ea78a9ac2dd5fe359588d57b79346b2b9
Jean Matheus Backes Kintenmac <jean.kistenmacher@pampa.compasso>
2020-12-14 18:01:18 -0300

    Finish Routes And Add Observables/Subjects

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,4 +1,4 @@
-import { EventEmitter, Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
 import { Recipe } from './recipe.model';
@@ -8,8 +8,6 @@ import { Recipe } from './recipe.model';
 })
 export class RecipeService {
   constructor(private shoppingListService: ShoppingListService) { }
-
-  recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
     new Recipe(
@@ -23,6 +21,10 @@ export class RecipeService {
 
   getRecipes() {
     return this.recipes.slice();
+  }
+
+  getRecipe(index: number) {
+    return this.recipes[index];
   }
 
   addIngredientsToShoppingList(ingredients: Ingredient[]) {
